204a3ee71545c26f6509edfe124890a0898bd35c
ErnarM <y_mukhtaruly@kbtu.kz>
2023-02-02 11:36:56 +0600

    TSIS 1

---
+++ Lab1/Exercises/3. Variables.py
@@ -0,0 +1,19 @@
+carname = "Volvo"
+
+x = 50
+
+x = 5
+y = 10
+print(x + y)
+
+z = x + y
+print(z)
+
+myfirst_name = "John" #Variable name
+
+x = y = z = "Orange"
+
+def myfunc():
+    global x
+    x = "fantastic"
+
